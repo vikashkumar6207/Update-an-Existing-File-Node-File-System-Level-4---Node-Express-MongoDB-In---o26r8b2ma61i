2de84b6a01173cb6a53394dc70ba84d774dec899
vikashkumar6207 <vktd828403@gmail.com>
2024-05-28 07:51:24 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const { appendFile } = require("fs");
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  await fs.appendFile(fileName, fileContent);
   
 };
-
+updateFile(fileName, "Is a online platform");
 module.exports = updateFile;
